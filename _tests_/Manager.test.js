b4e689fc57c774b6129c2438d9e73c4295f736d8
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-02-21 19:59:30 -0500

    added Manager test

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,15 @@
+const { test, expect } = require("@jest/globals")
+const Manager = require("../lib/Manager")
+
+test("manager should reciver all properties from Employee plus its own", () => {
+    
+    const manager1 = new Manager("Manager", 10, "Luis", "aap@gmail.com", 1044);
+
+    expect(manager1).toEqual({
+        role: "Manager",
+        id: 10,
+        name: "Luis",
+        email: "aap@gmail.com",
+        officeNumber: 1044
+    });
+});

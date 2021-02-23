4bdd116cb5c3919a7b585dc8c9de9a5cf95021f2
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-02-23 17:35:52 -0500

    added lines to generate index file

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const inquirer = require("inquirer");
 const fs = require('fs');
 const util = require('util');
 
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+
+
+
+// function to put generated file together
+async function init() {
+    try {
+        const data = await promptUser();
+        const readMeFile = mdFileTemplate(data);
+        await writeFileAsync("index1.html", readMeFile);
+    } catch (err) {
+        console.log(err)
+    }
+}
+init()

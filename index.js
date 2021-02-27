d4f8156fef224ee6a7bcff0cde887a89814af91c
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-02-26 21:56:44 -0500

    added manager prompts

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 const util = require('util');
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+cosnt Intern = require('./lib/Intern')
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-
-
+// Prompts for manager
+function promptManager(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Manager Name:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Manager ID:",
+            validate: function (value) {
+                var valid = !isNaN(parseFloat(value));
+                return valid || 'Please enter a number';
+              },
+              
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Manager Email:"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Manager Office Number:",
+            validate: function (value) {
+                var valid = !isNaN(parseFloat(value));
+                return valid || 'Please enter a number';
+              },
+        },
+        {
+            type: "list",
+            name: "another",
+            message: "Would you like to add an engineer or intern?",
+            choices: ["Engineer","Intern","None"]
+        }
+    ])
+}
 
 
 // function to put generated file together
